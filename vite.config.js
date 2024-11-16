64e5db281da0eb423feae7d603c35f8f51e14e9c
Jonas Cura <jonasm.cura@gmail.com>
2024-11-16 16:48:43 -0700

    adjust vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    legalComments: 'none',
+    minify: 'esbuild', // Use Terser instead of esbuild if you want more control
+  },
 })
